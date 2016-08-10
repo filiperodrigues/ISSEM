6c00acca16458a571509e41585bd726faa99848c
Wagner Esser <waesser@gmail.com>
2016-08-10 14:14:35 -0300

    Projeto inicial

---
+++ issem_project/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+from issem import views
+
+urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^issem/', include('issem.urls', namespace="issem")),
+    url(r'^$', views.index, name='index'),
+]
