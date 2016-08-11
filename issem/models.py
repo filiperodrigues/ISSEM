d6493eea414230dca6cc160aebcbcaa4e6dc27f4
KauanAmarante <kauan.amarante@hotmail.com>
2016-08-11 15:57:22 -0300

    Inserção Classe Benefício em Models

--- issem/models.py
+++ issem/models.py
@@ -1,3 +1,4 @@
+import datetime
 from django.db import models
 
 # Create your models here.
@@ -18,3 +19,18 @@ class Cid(models.Model):
 
     def __unicode__(self):
         return self.descricao_cid
+
+class Beneficios(models.Model):
+   concessao = models.BooleanField(default=0)
+   dt_inicial = models.DateField(initial=datetime.date.today)
+   dt_final = models.DateField(initial=datetime.date.today)
+   dt_retorno = models.DateField(initial=datetime.date.today)
+   dt_pericia = models.DateField(initial=datetime.date.today)
+   descricao = models.CharField(max_length=1000)
+   nr_portaria = models.IntegerField()
+   dt_portaria = models.DateField(initial=datetime.date.today)
+   max_salario = models.IntegerField()
+   observacao = models.CharField(max_length=1000)
+   carencia = models.IntegerField()
+
+
