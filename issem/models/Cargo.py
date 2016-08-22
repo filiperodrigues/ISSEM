f781538c6e1c305bd3257cf4420a59f77f02b2fa
Wagner Esser <waesser@gmail.com>
2016-08-22 14:33:35 -0300

    Adicionado padrão na estrutura de diretórios; Semantic implementado; CRUD Estado Civil e Tipo Sanguíneo.

---
+++ issem/models/Cargo.py
@@ -0,0 +1,8 @@
+#coding:utf-8
+from django.db import models
+
+class Cargo(models.Model):
+    nome = models.CharField(max_length=128, null=False)
+
+    def __str__(self):
+        return self.nome
---
+++ issem/models/Departamento.py
@@ -0,0 +1,9 @@
+#coding:utf-8
+import datetime
+from django.db import models
+
+class Departamento(models.Model):
+    nome = models.CharField(max_length=128, unique=True, null=False)
+
+    def __str__(self):
+        return self.nome
