f08ec410825a21de20f7b299ad7192e6959c3bb2
KauanAmarante <kauan.amarante@hotmail.com>
2016-09-14 13:44:21 -0300

    Modal

--- static/js/issem.js
+++ static/js/issem.js
@@ -1,20 +1,7 @@
-  $(document).ready(function(){
+$(document).ready(function () {
 
-    $('.fazul').click(function(){
 
-        id = $(this).attr('id')
-        $('.'+id).transition('slide down')
-    })
-
-  })
-
-  function formatar(mascara, documento){
-  var i = documento.value.length;
-  var saida = mascara.substring(0,1);
-  var texto = mascara.substring(i)
-
-  if (texto.substring(0,1) != saida){
-            documento.value += texto.substring(0,1);
-  }
-
-}
+    $('.ui.modal')
+        .modal('setting', 'closable', false)
+        .modal('attach events', '#cadastro-secretaria', 'show');
+    });
